02fbeb55897feb9ae77d54ac8d0d687c8e7ce1ea
EoinFitz03 <G00418325@atu.ie>
2023-11-01 17:14:55 +0000

    adding student 1

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,4 +1,53 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Student {
+    String name;
+    String email;
+    String course;
+
+
+    public Student(String name, String email, String course) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    public Student() {
+        this.name = "";
+        this.email = "";
+        this.course = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + "\nemail: " + email + "\ncourse: " + course);
+    }
+
+
+
 }
